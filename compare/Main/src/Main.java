f5727dcc4ffd5958a7fd2cb514c3f7b7b449c130
fu908301 <fu908301@gmail.com>
2018-10-04 17:21:10 +0800

    Upload Periodcity detection.

--- compare/Main/src/Main.java
+++ compare/Main/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args){
-        SuffixTrieTest suffixTrieTest = new SuffixTrieTest();
-        suffixTrieTest.test();
+        //SuffixTrieTest suffixTrieTest = new SuffixTrieTest();
+        //suffixTrieTest.test();
+        int input[] = {2, 3, 7, 9, 11, 15};
+        int size = 6;
+        int threshold = 2;
+        int length = 16;
+        Periodicity_detection p = new Periodicity_detection(input, size, threshold, length);
+        p.algorithm();
+        p.print();
     }
 }
---
+++ compare/Main/src/Periodicity_detection.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+public class Periodicity_detection {
+    private int[] occ;
+    private int size;
+    private int threshold;
+    private int length;
+    private ArrayList<Integer> V;
+    private ArrayList<Integer> OP;
+    Periodicity_detection(){
+    }
+    Periodicity_detection(int occ[],int size, int threshold, int length){
+        this.size = size;
+        this.occ = new int [this.size];
+        for(int i = 0; i < size; i++){
+            this.occ[i] = occ[i];
+        }
+        this.threshold = threshold;
+        this.length = length;
+        V = new ArrayList<>();
+        OP = new ArrayList<>();
+    }
+
+    public void algorithm(){
+        int st, sigma, period, PP, support;
+        for (int i = 1; i <= size - 1; i++){
+            st = occ[i - 1];
+            for(int j = i; j <= size - 1; j++){
+                sigma = occ[j];
+                period = sigma - st;
+                V.clear();
+                V.add(st);
+                V.add(sigma);
+                for(int m = j + 1; m <= size - 1; m++){
+                    if((occ[m] - st) % period == 0){
+                        V = union(V, occ[m]);
+                    }
+                }
+                PP = (length - st + 1) / period;
+                support = V.size() / PP;
+                if(support >= threshold){
+                    OP = union(OP, V);
+                }
+            }
+        }
+    }
+    public void print(){
+        for(int i = 0; i < OP.size(); i++){
+            System.out.println(OP.get(i));
+        }
+    }
+    private ArrayList<Integer> union(ArrayList<Integer> input1, int input2){
+        ArrayList<Integer> result = new ArrayList<>();
+        result.addAll(input1);
+        if(!result.contains(input2)){
+            result.add(input2);
+        }
+        return result;
+    }
+    private ArrayList<Integer> union(ArrayList<Integer> input1, ArrayList<Integer> input2){
+        ArrayList<Integer> result = new ArrayList<>();
+        result.addAll(input1);
+        for(int i = 0; i < input2.size(); i++){
+            if(!result.contains(input2.get(i))){
+                result.add(input2.get(i));
+            }
+        }
+        return result;
+    }
+}
